286d6f23a94b3a43fe69aaed2d06740f630a12e6
ram2288 <88101339+ram2288@users.noreply.github.com>
2023-12-30 10:28:05 -0400

    entidades
    arreglo de entidades

---
+++ src/main/java/com/dev/eispdm/entidades/Usuario.java
@@ -0,0 +1,31 @@
+package com.dev.eispdm.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "aulas")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+    private String nomUsuario;
+    private String apePaterno;
+    private String apeMaterno;
+    private String ci;
+    private String usuario;
+    private String pass;
+    @ManyToOne
+    @JoinColumn(name = "id_rol", nullable = false)
+    private Rol rol;
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Prestamo> prestamos;
+}
